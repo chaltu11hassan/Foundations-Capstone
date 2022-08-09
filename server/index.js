211694317923f6f321bc0bc5f43e7cdc827da142
Chaltu Hassan <chaltu11hassan@gmail.com>
2022-08-08 23:26:35 -0500

    ready

--- server/index.js
+++ server/index.js
@@ -49,9 +49,6 @@ app.use((req,res) =>{
     res.redirect('/home')
 })
 
-// const port = process.env.SERVER_PORT || 4000
-
-// app.listen(port, ()=> console.log(`server running on ${port}`));
 
 const port = process.env.PORT || 4000
 
